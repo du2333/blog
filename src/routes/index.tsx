d6fd77671cc07359d4a34fa7e4e6fd3b14464e0c
Zephyrrr <kuangdadu@gmail.com>
2025-10-23 22:07:55 -0700

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({ component: App });
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Hello World</h1>
+    </div>
+  );
+}
