090a7e645a7e2c62a423147126162b6e13b3782c
Zephyrrr <kuangdadu@gmail.com>
2025-10-23 23:00:32 -0700

    integrate cloudflare

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import { env } from "cloudflare:workers";
 
-export const Route = createFileRoute("/")({ component: App });
+const getOK = createServerFn().handler(() => {
+  return env.OK;
+});
+
+export const Route = createFileRoute("/")({
+  loader: () => getOK(),
+  component: App,
+});
 
 function App() {
+  const ok = Route.useLoaderData();
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold">Hello World</h1>
+      <p>The value of OK is: {ok ?? "No value found"}</p>
     </div>
   );
 }
