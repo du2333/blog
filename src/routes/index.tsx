2761feb280dbf2422cacf9ca520112011a65e67c
Zephyrrr <kuangdadu@gmail.com>
2025-10-24 00:06:55 -0700

    Add @tanstack/devtools-vite to remove devtools in production builds

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
-import { env } from "cloudflare:workers";
-
-const getOK = createServerFn().handler(() => {
-  return env.OK;
-});
 
 export const Route = createFileRoute("/")({
-  loader: () => getOK(),
   component: App,
 });
 
 function App() {
-  const ok = Route.useLoaderData();
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">Hello World</h1>
-      <p>The value of OK is: {ok ?? "No value found"}</p>
+      <h1 className="text-2xl font-bold">Hello World 2</h1>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { devtools } from "@tanstack/devtools-vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
@@ -19,6 +20,7 @@ const config = defineConfig({
         name: "ssr",
       },
     }),
+    devtools(),
   ],
 });
 
