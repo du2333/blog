71ab991f16b475006c9ffa62e009794abf67ad35
Zephyrrr <kuangdadu@gmail.com>
2026-01-01 00:11:04 -0800

    Add Prettier configuration and dependencies: Introduce .prettierignore, add Prettier to package.json, and create a Prettier configuration file for consistent code formatting.

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "all",
+  singleQuote: false,
+  tabWidth: 2,
+  semi: true,
+};
+
+export default config;
