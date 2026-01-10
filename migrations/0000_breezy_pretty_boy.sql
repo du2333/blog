65e5c16050b5ef930f3055932d7f06cfc0e5dd1d
Zephyrrr <kuangdadu@gmail.com>
2026-01-09 18:58:46 -0800

    rebuild db schema, fix cascading issue

--- migrations/0000_breezy_pretty_boy.sql
+++ migrations/0000_breezy_pretty_boy.sql
@@ -1,3 +1,24 @@
+CREATE TABLE `comments` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`content` text,
+	`root_id` integer,
+	`reply_to_comment_id` integer,
+	`status` text DEFAULT 'verifying' NOT NULL,
+	`ai_reason` text,
+	`post_id` integer NOT NULL,
+	`user_id` text NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
+	FOREIGN KEY (`root_id`) REFERENCES `comments`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`reply_to_comment_id`) REFERENCES `comments`(`id`) ON UPDATE no action ON DELETE set null,
+	FOREIGN KEY (`post_id`) REFERENCES `posts`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE set null
+);
+--> statement-breakpoint
+CREATE INDEX `comments_post_root_created_idx` ON `comments` (`post_id`,`root_id`,`created_at`);--> statement-breakpoint
+CREATE INDEX `comments_user_created_idx` ON `comments` (`user_id`,`created_at`);--> statement-breakpoint
+CREATE INDEX `comments_status_created_idx` ON `comments` (`status`,`created_at`);--> statement-breakpoint
+CREATE INDEX `comments_global_created_idx` ON `comments` (`created_at`);--> statement-breakpoint
 CREATE TABLE `media` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`key` text NOT NULL,
